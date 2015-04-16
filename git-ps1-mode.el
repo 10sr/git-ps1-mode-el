39bdd3587178a6f6d59132122ce6d2a8735bf566
10sr <8slashes+git@gmail.com>
2015-04-16 17:16:32 +0900

    Initial

--- git-ps1-mode.el
+++ git-ps1-mode.el
@@ -9,122 +9,122 @@
 
 ;;; Code:
 
-;; ===変数定義==================================================
+(defvar git-ps1-mode-string "[......]"
+  "String to show in mode-line.")
 
-;; モードラインに表示する文字列
-;; Default値はプロセス実行前の表示
-(defvar modeline-git-branch-string "[......]")
-
-;; バッファ内Processオブジェクト
-;; non-nilのときは新しいプロセスを実行しない
-(defvar modeline-git-branch-process nil)
+(defvar git-ps1-mode-process nil
+  "Existing process object or nil.")
 
 ;; make local-variable
-(make-variable-buffer-local 'modeline-git-branch-string)
-(make-variable-buffer-local 'modeline-git-branch-process)
+(make-variable-buffer-local 'git-ps1-mode-string)
+(make-variable-buffer-local 'git-ps1-mode-process)
 
 
-;; ===関数定義==================================================
-
-;; プロセスの実行を予約する
-(defun modeline-git-branch-schedule-update (buffer &optional force)
+(defun git-ps1-mode-schedule-update (buffer &optional force)
+  "Register process execution timer to run in BUFFER.
+"
   (run-with-idle-timer
-   0.0 nil #'modeline-git-branch-run-process buffer force))
+   0.0 nil #'git-ps1-mode-run-process buffer force))
 
-;; プロセスを立ち上げてブランチ名を取得する
-(defun modeline-git-branch-run-process (buffer force)
+(defun git-ps1-mode-run-process (buffer force)
+  "Run git process in BUFFER and get branch name.
+Set FORCE to non-nil to skip buffer check."
   (when (or (and force (buffer-live-p buffer))
             (eq buffer (current-buffer)))
     (with-current-buffer buffer
-      (unless modeline-git-branch-process
+      (unless git-ps1-mode-process
         (let ((process-connection-type nil))
-          (setq modeline-git-branch-process
-                (start-process "modeline-git-branch" buffer
+          (setq git-ps1-mode-process
+                (start-process "git-ps1-mode" buffer
                                "git" "symbolic-ref" "HEAD"))
-          (set-process-filter modeline-git-branch-process
-                              'modeline-git-branch-update-modeline)
-          (set-process-sentinel modeline-git-branch-process
-                                'modeline-git-branch-clear-process)
-          (set-process-query-on-exit-flag modeline-git-branch-process
+          (set-process-filter git-ps1-mode-process
+                              'git-ps1-mode-update-modeline)
+          (set-process-sentinel git-ps1-mode-process
+                                'git-ps1-mode-clear-process)
+          (set-process-query-on-exit-flag git-ps1-mode-process
                                           nil))))))
 
-;; プロセスからの出力を整形してモードラインを更新する
-(defun modeline-git-branch-update-modeline (process output)
+(defun git-ps1-mode-update-modeline (process output)
+  "Format output of `git-ps1-mode-run-process' and update modeline.
+This function is passed as an argument for `set-process-filter': see
+document of that function for details about PROCESS and OUTPUT."
   (when (buffer-live-p (process-buffer process))
     (with-current-buffer (process-buffer process)
       (cond ((string= "fatal: ref HEAD is not a symbolic ref"
                       (substring output 0 -1))
-             (setq modeline-git-branch-string "[no-branch]"))
+             (setq git-ps1-mode-string "[no-branch]"))
             ((string-match "^fatal" output)
-             (setq modeline-git-branch-string "[no-repo]"))
+             (setq git-ps1-mode-string "[no-repo]"))
             (t
-             (setq modeline-git-branch-string
+             (setq git-ps1-mode-string
                    (format "[%s]" (substring output 11 -1)))))
       (force-mode-line-update))))
 
-;; 終了したプロセスをクリアする
-(defun modeline-git-branch-clear-process (process state)
+(defun git-ps1-mode-clear-process (process state)
+  "Clear exitted process.
+This function is passed as an argument for `set-process-sentinel': see
+document of that function for details about PROCESS and STATE."
   (when (buffer-live-p (process-buffer process))
     (with-current-buffer (process-buffer process)
-      (setq modeline-git-branch-process nil))))
+      (setq git-ps1-mode-process nil))))
 
 
 ;; ===hook用関数定義============================================
 
 ;; hook#1 after-change-major-mode-hook, after-save-hook
-(defun modeline-git-branch-update-current ()
+(defun git-ps1-mode-update-current ()
   (unless (minibufferp (current-buffer))
-    (modeline-git-branch-schedule-update (current-buffer) t)))
+    (git-ps1-mode-schedule-update (current-buffer) t)))
 
 ;; hook#2 select-window-functions
-(defun modeline-git-branch-update-when-select-window
+(defun git-ps1-mode-update-when-select-window
   (before-win after-win)
   (unless (minibufferp (window-buffer after-win))
-    (modeline-git-branch-schedule-update (window-buffer after-win))))
+    (git-ps1-mode-schedule-update (window-buffer after-win))))
 
 ;; hook#3 set-selected-window-buffer-functions
-(defun modeline-git-branch-update-when-set-window-buffer
+(defun git-ps1-mode-update-when-set-window-buffer
   (before-buf win after-buf)
   (unless (minibufferp after-buf)
-    (modeline-git-branch-schedule-update after-buf)))
+    (git-ps1-mode-schedule-update after-buf)))
 
 
 ;; ===マイナーモード定義========================================
 
-(defun modeline-git-branch-enable ()
+(defun git-ps1-mode-enable ()
   (setcar (or (member '(vc-mode vc-mode) mode-line-format)
               (list nil))
-          'modeline-git-branch-string)
-  (modeline-git-branch-update-current)
+          'git-ps1-mode-string)
+  (git-ps1-mode-update-current)
   (add-hook 'after-change-major-mode-hook
-            'modeline-git-branch-update-current)
+            'git-ps1-mode-update-current)
   (add-hook 'after-save-hook
-            'modeline-git-branch-update-current)
+            'git-ps1-mode-update-current)
   (add-hook 'select-window-functions
-            'modeline-git-branch-update-when-select-window)
+            'git-ps1-mode-update-when-select-window)
   (add-hook 'set-selected-window-buffer-functions
-            'modeline-git-branch-update-when-set-window-buffer))
+            'git-ps1-mode-update-when-set-window-buffer))
 
-(defun modeline-git-branch-disable ()
-  (setcar (or (memq 'modeline-git-branch-string mode-line-format)
+(defun git-ps1-mode-disable ()
+  (setcar (or (memq 'git-ps1-mode-string mode-line-format)
               (list nil))
           '(vc-mode vc-mode))
   (remove-hook 'after-change-major-mode-hook
-               'modeline-git-branch-update-current)
+               'git-ps1-mode-update-current)
   (remove-hook 'after-save-hook
-               'modeline-git-branch-update-current)
+               'git-ps1-mode-update-current)
   (remove-hook 'select-window-functions
-               'modeline-git-branch-update-when-select-window)
+               'git-ps1-mode-update-when-select-window)
   (remove-hook 'set-selected-window-buffer-functions
-               'modeline-git-branch-update-when-set-window-buffer))
+               'git-ps1-mode-update-when-set-window-buffer))
 
-(define-minor-mode modeline-git-branch-mode
+(define-minor-mode git-ps1-mode-mode
   "[git-branch]"
-  :group 'modeline-git-branch
+  :group 'git-ps1-mode
   :global t
-  (if modeline-git-branch-mode
-      (modeline-git-branch-enable)
-    (modeline-git-branch-disable))
+  (if git-ps1-mode-mode
+      (git-ps1-mode-enable)
+    (git-ps1-mode-disable))
   (force-mode-line-update t))
 
 (provide 'git-ps1-mode)
