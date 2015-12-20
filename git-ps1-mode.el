01981c5f5fae92aa258b9cf4a6831f4b8c86891a
10sr <8slashes+git@gmail.com>
2015-12-20 15:25:26 +0900

    Reorder and reindent

--- git-ps1-mode.el
+++ git-ps1-mode.el
@@ -45,8 +45,8 @@
 
 ;; * `git-ps1-mode-lighter-text-format`
 
-;;   Format string for `git-ps1-mode` lighter (mode-name). By default it is set to
-;;   `" [GIT:%s]"`.
+;;   Format string for `git-ps1-mode` lighter (mode-name). By default it is set
+;;   to `" [GIT:%s]"`.
 
 ;; * `git-ps1-mode-ps1-file`
 
@@ -69,7 +69,6 @@
 ;;   By default it is set to `2`.
 
 
-;; TODO: Instruct users to set git-ps1-mode-ps1-file
 ;; TODO: Use same status text if `git rev-parse --show-toplevel` is same
 
 ;;; Code:
@@ -80,24 +79,6 @@
   "File path that contains \"__git_ps1\" definition.
 If set to nil, try to find the definition from
  `git-ps1-mode-ps1-file-candidates-list' by `git-ps1-mode-find-ps1-file'.")
-
-(defvar git-ps1-mode-ps1-file-candidates-list
-  '(
-    "/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh"
-    "/usr/share/git/completion/git-prompt.sh"
-    "/opt/local/share/doc/git-core/contrib/completion/git-prompt.sh"
-    "/etc/bash_completion.d/git"
-    "/etc/bash_completion.d/git-prompt"
-    "/opt/local/share/git-core/git-prompt.sh"
-    "/opt/local/etc/bash_completion.d/git"
-    )
-  "List of candidates that may contain \"__git_ps1\" definition.")
-
-(defvar git-ps1-mode--ps1-file-candidates-found
-  nil
-  "Script with __git_ps1 definition.
-This variable is used when `git-ps1-mode-ps1-file' is set to nil.")
-
 
 ;; variables to configure __git_ps1
 ;; TODO: use defcustom
@@ -122,16 +103,6 @@ This variable is used when `git-ps1-mode-ps1-file' is set to nil.")
   "Value of GIT_PS1_SHOWUPSTREAM when running __git_ps1.")
 
 
-
-(defvar git-ps1-mode-process nil
-  "Existing process object or nil.")
-(make-variable-buffer-local 'git-ps1-mode-process)
-
-(defvar git-ps1-mode-lighter-text
-  ""
-  "Lighter text for `git-ps1-mode'.  This variable is for internal usage.")
-(make-variable-buffer-local 'git-ps1-mode-lighter-text)
-
 ;; TODO: use defcustom
 (defvar git-ps1-mode-lighter-text-format " [GIT:%s]"
   "Format for `git-ps1-mode' lighter.
@@ -141,6 +112,34 @@ String \"%s\" will be replaced with the output of \"__git_ps1 %s\".")
 ;; TODO: use defcustom
 (defvar git-ps1-mode-idle-interval 2
   "If Emacs is idle for this seconds `git-ps1-mode' will update lighter text.")
+
+
+(defvar git-ps1-mode-ps1-file-candidates-list
+  '(
+    "/Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh"
+    "/usr/share/git/completion/git-prompt.sh"
+    "/opt/local/share/doc/git-core/contrib/completion/git-prompt.sh"
+    "/etc/bash_completion.d/git"
+    "/etc/bash_completion.d/git-prompt"
+    "/opt/local/share/git-core/git-prompt.sh"
+    "/opt/local/etc/bash_completion.d/git"
+    )
+  "List of candidates that may contain \"__git_ps1\" definition.")
+
+(defvar git-ps1-mode--ps1-file-candidates-found
+  nil
+  "Script with __git_ps1 definition.
+This variable is used when `git-ps1-mode-ps1-file' is set to nil.")
+
+
+(defvar git-ps1-mode-process nil
+  "Existing process object or nil.")
+(make-variable-buffer-local 'git-ps1-mode-process)
+
+(defvar git-ps1-mode-lighter-text
+  ""
+  "Lighter text for `git-ps1-mode'.  This variable is for internal usage.")
+(make-variable-buffer-local 'git-ps1-mode-lighter-text)
 
 (defvar git-ps1-mode-idle-timer-object nil
   "Idle timer object returned from `run-with-idle-timer'.")
@@ -175,49 +174,41 @@ This function returns the path of the first file foundor nil if none.  If LIST
                 (git-ps1-mode-find-ps1-file (cdr l)))))))
 
 
-(defun git-ps1-mode-schedule-update (buffer &optional force)
-  "Register process execution timer.
-Arguments BUFFER and FORCE will be passed to `git-ps1-mode-run-proess'."
-  (when (and (or git-ps1-mode-ps1-file
-                 git-ps1-mode--ps1-file-candidates-found)
-             (file-directory-p default-directory))
-    (run-with-idle-timer
-     0.0 nil #'git-ps1-mode-run-process buffer force)))
-
 (defun git-ps1-mode-run-process (buffer force)
   "Run git process in BUFFER and get branch name.
 Set FORCE to non-nil to skip buffer check."
   (when (file-directory-p default-directory)
-  (when (or (and force (buffer-live-p buffer))
-            (eq buffer (current-buffer)))
-    (with-current-buffer buffer
-      (unless git-ps1-mode-process
-        (let ((process-environment `(,(concat "GIT_PS1_SHOWDIRTYSTATE="
-                                              git-ps1-mode-showdirtystate)
-                                     ,(concat "GIT_PS1_SHOWSTASHSTATE="
-                                              git-ps1-mode-showstashstate)
-                                     ,(concat "GIT_PS1_SHOWUNTRACKEDFILES="
-                                              git-ps1-mode-showuntrackedfiles)
-                                     ,(concat "GIT_PS1_SHOWUPSTREAM="
-                                              git-ps1-mode-showupstream)
-                                     ,@process-environment))
-              (process-connection-type nil))
-          (setq git-ps1-mode-process
-                (start-process "git-ps1-mode" buffer
-                               ;; TODO: parameterize bash executable
-                               "bash" "-s"))
-          (set-process-filter git-ps1-mode-process
-                              'git-ps1-mode-update-modeline)
-          (set-process-sentinel git-ps1-mode-process
-                                'git-ps1-mode-clear-process)
-          (set-process-query-on-exit-flag git-ps1-mode-process
-                                          nil)
-          (process-send-string git-ps1-mode-process
-                               (format ". \"%s\"; __git_ps1 %s"
-                                       (or git-ps1-mode-ps1-file
-                                           git-ps1-mode--ps1-file-candidates-found)
-                                       "%s"))
-          (process-send-eof git-ps1-mode-process)))))))
+    (when (or (and force
+                   (buffer-live-p buffer))
+              (eq buffer (current-buffer)))
+      (with-current-buffer buffer
+        (unless git-ps1-mode-process
+          (let ((process-environment `(,(concat "GIT_PS1_SHOWDIRTYSTATE="
+                                                git-ps1-mode-showdirtystate)
+                                       ,(concat "GIT_PS1_SHOWSTASHSTATE="
+                                                git-ps1-mode-showstashstate)
+                                       ,(concat "GIT_PS1_SHOWUNTRACKEDFILES="
+                                                git-ps1-mode-showuntrackedfiles)
+                                       ,(concat "GIT_PS1_SHOWUPSTREAM="
+                                                git-ps1-mode-showupstream)
+                                       ,@process-environment))
+                (process-connection-type nil))
+            (setq git-ps1-mode-process
+                  (start-process "git-ps1-mode" buffer
+                                 ;; TODO: parameterize bash executable
+                                 "bash" "-s"))
+            (set-process-filter git-ps1-mode-process
+                                'git-ps1-mode-update-modeline)
+            (set-process-sentinel git-ps1-mode-process
+                                  'git-ps1-mode-clear-process)
+            (set-process-query-on-exit-flag git-ps1-mode-process
+                                            nil)
+            (process-send-string git-ps1-mode-process
+                                 (format ". \"%s\"; __git_ps1 %s"
+                                         (or git-ps1-mode-ps1-file
+                                             git-ps1-mode--ps1-file-candidates-found)
+                                         "%s"))
+            (process-send-eof git-ps1-mode-process)))))))
 
 (defun git-ps1-mode-update-modeline (process output)
   "Format output of `git-ps1-mode-run-process' and update modeline.
@@ -240,16 +231,23 @@ document of that function for details about PROCESS and STATE."
 
 
 
-;; Functions for hooks
-
-;; hook#1 after-change-major-mode-hook, after-save-hook,
+;; For after-change-major-mode-hook, after-save-hook,
 ;; window-configuration-change-hook, run-with-idle-timer
 (defun git-ps1-mode-update-current ()
   "Update status text immediately."
   (interactive)
   (when (file-directory-p default-directory)
-  (unless (minibufferp (current-buffer))
-    (git-ps1-mode-schedule-update (current-buffer) t))))
+    (unless (minibufferp (current-buffer))
+      (git-ps1-mode-schedule-update (current-buffer) t))))
+
+(defun git-ps1-mode-schedule-update (buffer &optional force)
+  "Register process execution timer.
+Arguments BUFFER and FORCE will be passed to `git-ps1-mode-run-proess'."
+  (when (and (or git-ps1-mode-ps1-file
+                 git-ps1-mode--ps1-file-candidates-found)
+             (file-directory-p default-directory))
+    (run-with-idle-timer
+     0.0 nil #'git-ps1-mode-run-process buffer force)))
 
 
 
